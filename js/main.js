4302f0fd868789d0e516abb1e91a6c5151d170bc
DJ Neill <dj.neill856@gmail.com>
2023-06-08 01:06:07 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ const info = document.querySelector("h3");
 const title = document.querySelector('h2')
 const copy = document.querySelector('p')
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
   getFetch()
 })
 // event listener for button click
@@ -21,59 +21,59 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 // event listener for enter pressed
 let btn = document.getElementById('btn');
-   
-   // when the btn is clicked print info in console 
-   btn.addEventListener('click', (ev)=>{
-     getFetch()
-   });
 
-   document.addEventListener('keypress', (event)=>{
+// when the btn is clicked print info in console 
+btn.addEventListener('click', (ev) => {
+  getFetch()
+});
 
-     // event.keyCode or event.which  property will have the code of the pressed key
-     let keyCode = event.keyCode ? event.keyCode : event.which;
+document.addEventListener('keypress', (event) => {
 
-     // 13 points the enter key
-     if(keyCode === 13) {
-       // call click function of the buttonn 
-       btn.click();
-     }
-       
-   });
+  // event.keyCode or event.which  property will have the code of the pressed key
+  let keyCode = event.keyCode ? event.keyCode : event.which;
+
+  // 13 points the enter key
+  if (keyCode === 13) {
+    // call click function of the buttonn 
+    btn.click();
+  }
+
+});
 
 // get date from input field
 function getFetch() {
-    const choice = document.querySelector('input').value
-    console.log(choice)
+  const choice = document.querySelector('input').value
+  console.log(choice)
 
-// nasa apod api url with date from input
-const url = `https://api.nasa.gov/planetary/apod?api_key=knyEdcu9bPxCjTZ0hF7s2ZM8COfYk2spxCtWtEX0&date=${choice}`
+  // nasa apod api url with date from input
+  const url = `https://api.nasa.gov/planetary/apod?api_key=knyEdcu9bPxCjTZ0hF7s2ZM8COfYk2spxCtWtEX0&date=${choice}`
 
-// fetch response from api
-fetch(url)
-  .then(res => res.json()) //parse response as JSON
-  .then(data => {
+  // fetch response from api
+  fetch(url)
+    .then(res => res.json()) //parse response as JSON
+    .then(data => {
       console.log(data);
       // display image in the dom
-      if(data.media_type ==='image') {
-          // let's first set the source, and then display the image
-          image.classList.remove("none")
-          image.src = data.hdurl;
-          
+      if (data.media_type === 'image') {
+        // let's first set the source, and then display the image
+        image.classList.remove("none")
+        image.src = data.hdurl;
 
-          // let's hide the iframe first, and then remove the source
-          ifr.classList.add("none")
-          ifr.src = "";
-      // display video in the dom
+
+        // let's hide the iframe first, and then remove the source
+        ifr.classList.add("none")
+        ifr.src = "";
+        // display video in the dom
       } else if (data.media_type === 'video') {
-          // similar as above - hide first, remove source, and the opposite
-          // for iframe
-          image.classList.add("none")
-          image.src = "";
+        // similar as above - hide first, remove source, and the opposite
+        // for iframe
+        image.classList.add("none")
+        image.src = "";
 
-          ifr.classList.remove("none")
-          ifr.src = data.url;
+        ifr.classList.remove("none")
+        ifr.src = data.url;
       } else {
-          alert('Error, please try again')
+        alert('Error, please try again')
       }
 
       // display explanation of image / video
@@ -82,8 +82,9 @@ fetch(url)
       title.innerText = data.title
       // display copyright of image / video
       copy.innerText = data.copyright
-  })
-  .catch(err => {
+    })
+    .catch(err => {
+      document.getElementById('title').innerHTML = 'The NASA picture of the day server is currently unavailable (because it is overloaded or down for maintenance). Generally, this is a temporary state. Please try again later.'
       console.log(`error ${err}`)
-  })
+    })
 }
